78ac560cdb479426304aaf75b4b1fb1e3bbd3551
sainath <bandasainathreddy@gmail.com>
2024-10-10 15:43:32 -0500

    first commit

---
+++ src/components/notFound/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Hero from "../hero/Hero";
+const NotFound=({movies})=>{
+  return(
+  <div>
+    This page could not be found!!!
+  </div>
+  )
+}
+export default NotFound;
